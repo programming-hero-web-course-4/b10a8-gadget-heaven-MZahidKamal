eafae881dc9767f00e20d590082ef29a4023586d
MZahidKamal <kamal.md.zahid@gmail.com>
2024-11-03 15:00:55 +0100

    Project Created using React and Vite. Installed TailwindCSS and React Router.

---
+++ src/Routes/Root.jsx
@@ -0,0 +1,11 @@
+const Root = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Routes/Root.jsx'
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>
+    },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
+)
